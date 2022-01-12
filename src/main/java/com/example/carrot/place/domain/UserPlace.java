0d7d4487caca9f64e3d3f5c97b0ef770cd219900
imaspear <rjsckdd12@gmail.com>
2022-01-13 00:12:53 +0900

    feature : place 기능 도메인 설계

---
+++ src/main/java/com/example/carrot/place/domain/UserPlace.java
@@ -0,0 +1,11 @@
+package com.example.carrot.place.domain;
+
+/**
+ * Created by Imaspear on 2022-01-12
+ * Blog : https://imspear.tistory.com/
+ * Github : https://github.com/Imaspear
+ */
+public class UserPlace {
+    private Long user_id;
+    private Long place_code;
+}
