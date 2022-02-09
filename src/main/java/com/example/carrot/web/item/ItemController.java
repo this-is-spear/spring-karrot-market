b50addafd1ea216ff5a10a32f6a45d9da10b605d
Imaspear <rjsckdd12@gmail.com>
2022-02-09 14:16:28 +0900

    feature : item 입력시 오류 잘못된 요청 오류 반환

--- src/main/java/com/example/carrot/web/item/ItemController.java
+++ src/main/java/com/example/carrot/web/item/ItemController.java
@@ -12,9 +12,11 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.List;
@@ -32,9 +34,12 @@ public class ItemController {
     }
 
     @PostMapping("/items/new")
-    public String saveItem(@ModelAttribute ItemForm form, RedirectAttributes redirectAttributes) throws IOException {
+    public String saveItem(@Valid @ModelAttribute("itemForm") ItemForm form, BindingResult bindingResult, RedirectAttributes redirectAttributes) throws IOException {
         log.debug("[{}][{}] item form", MDC.get(CheckThreadLog.LOG_ID), form.toString());
-
+        if (bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
+            return "items/item-form";
+        }
         Item item = saveItem(form);
 
         redirectAttributes.addAttribute("itemId", item.getId());
@@ -58,13 +63,19 @@ public class ItemController {
     @GetMapping("/items/{id}")
     public String items(@PathVariable Long id, Model model) {
         Item item = itemRepository.findById(id);
+        if (item == null) {
+            log.debug("[{}][{}] not found itemId", MDC.get(CheckThreadLog.LOG_ID), id);
+            return "redirect:/";
+        }
         model.addAttribute("item", item);
         return "items/item-view";
     }
 
     @ResponseBody
     @GetMapping("/images/{filename}")
-    public Resource downloadImage(@PathVariable String filename) throws MalformedURLException {
+    public Resource showImage(@PathVariable String filename) throws MalformedURLException {
+        log.debug("[{}][{}] Show Image", MDC.get(CheckThreadLog.LOG_ID), filename);
+
         return new UrlResource("file:" + fileStore.getFullPath(filename));
     }
 
--- src/main/java/com/example/carrot/web/item/ItemForm.java
+++ src/main/java/com/example/carrot/web/item/ItemForm.java
@@ -4,14 +4,31 @@ import com.example.carrot.domain.item.category.Category;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Data
 public class ItemForm {
     private Long itemId;
+
+    @NotNull
+    @NotEmpty
     private String itemName;
+
+    @NotNull
+    @NotEmpty
     private String itemContent;
+
+    @Max(value = 10000000)
+    @Min(value = 0)
+    @NotNull
     private int itemPrice;
+
+    @NotNull
     private Category itemCategory;
+
     private List<MultipartFile> imageFiles;
 }
